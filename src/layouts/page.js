e87214cb15c1cb5be978127d983cafb2693024b1
Stackbit Code Editor <projects@stackbit.com>
2021-09-28 20:10:40 +0000

    page.js: changed

--- src/layouts/page.js
+++ src/layouts/page.js
@@ -15,8 +15,6 @@ export default class Page extends React.Component {
         const imageAlt = _.get(page, 'image_alt', '');
         const markdownContent = _.get(page, 'markdown_content');
 
-        image.style.width=50;
-        image.style.height=auto;
 
         return (
             <Layout page={page} config={config}>
